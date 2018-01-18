f53ceac6d90939e23a6a0ee663d7336e1c1a31a8
sonu29081984 <saurav.bansal.kbl@gmail.com>
2018-01-18 01:19:48 -0500

    Update Webhook.py

--- Webhook.py
+++ Webhook.py
@@ -21,22 +21,22 @@ def Webhook():
 
 
 def makeResponse(req):
-    # if req.get("result").get("action") != "GetOrderStatus":
-    #     return {}
-    # result = req.get("result")
-    # parameters = result.get("parameters")
-    # Brand = parameters.get("Brand")
-    # OrderNumber = parameters.get("OrderNumber")
-    # url = 'https://cloud.10-4.com/atlas/frexapi/shipment/' + OrderNumber
-    # Pass='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYjI1NTA4Zi1mNTQwLTRjNDItYjQwNi0zNzMyMDVhZThmOTkiLCJodHRwOi8vMTAtNC5jb20vY2xhaW1zL3VzZXJJZCI6IjIzNTEiLCJpc3MiOiJodHRwczovL2Nsb3VkLjEwLTQuY29tIiwiYXVkIjoiaHR0cHM6Ly9jbG91ZC4xMC00LmNvbSIsImV4cCI6MjE0NzM5NTgxMCwibmJmIjoxNTE2MjQzODEwfQ.YtnGLxWij4sZ-NhAiEVLXgbUXrKZmsKb1uZn_ISH5GU'
-    # headers = {
-    #     'Authorization': Pass}
-    # if OrderNumber is None:
-    #     return None
-    # r = requests.get(url, headers=headers)
-    # json_object = r.json()
-    # ShareUrl = json_object['ShareUrl']
-    speech = "The Order status can be obtained by clicking the below URL :: " #+ ShareUrl
+    if req.get("result").get("action") != "GetOrderStatus":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    Brand = parameters.get("Brand")
+    OrderNumber = parameters.get("OrderNumber")
+    url = 'https://cloud.10-4.com/atlas/frexapi/shipment/' + OrderNumber
+    Pass='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYjI1NTA4Zi1mNTQwLTRjNDItYjQwNi0zNzMyMDVhZThmOTkiLCJodHRwOi8vMTAtNC5jb20vY2xhaW1zL3VzZXJJZCI6IjIzNTEiLCJpc3MiOiJodHRwczovL2Nsb3VkLjEwLTQuY29tIiwiYXVkIjoiaHR0cHM6Ly9jbG91ZC4xMC00LmNvbSIsImV4cCI6MjE0NzM5NTgxMCwibmJmIjoxNTE2MjQzODEwfQ.YtnGLxWij4sZ-NhAiEVLXgbUXrKZmsKb1uZn_ISH5GU'
+    headers = {
+        'Authorization': Pass}
+    if OrderNumber is None:
+        return None
+    r = requests.get(url, headers=headers)
+    json_object = r.json()
+    ShareUrl = json_object['ShareUrl']
+    speech = "The Order status can be obtained by clicking the below URL :: " + ShareUrl
     return {
         "speech": speech,
         "displayText": speech,
