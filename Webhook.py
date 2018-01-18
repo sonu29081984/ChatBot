97075b575252177a83282729ed9b2b28d930c58d
sonu29081984 <saurav.bansal.kbl@gmail.com>
2018-01-18 01:17:39 -0500

    Update Webhook.py

--- Webhook.py
+++ Webhook.py
@@ -13,11 +13,12 @@ app = Flask(__name__)
 def Webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
-    res = processRequest(req)
+    res = makeResponse(req)
     res = json.dumps(res, indent=4)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
+
 
 def makeResponse(req):
     # if req.get("result").get("action") != "GetOrderStatus":
