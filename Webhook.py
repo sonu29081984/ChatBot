5be540180d39e2bf803ca5f68debcf27becef88e
sonu29081984 <saurav.bansal.kbl@gmail.com>
2018-01-17 23:21:50 -0600

    ss

---
+++ Webhook.py
@@ -0,0 +1,58 @@
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeResponse(req):
+    if req.get("result").get("action") != "GetOrderStatus":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    Brand = parameters.get("Brand")
+    OrderNumber = parameters.get("OrderNumber")
+    url = 'https://cloud.10-4.com/atlas/frexapi/shipment/' + OrderNumber
+    headers = {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYjI1NTA4Zi1mNTQwLTRjNDItYjQwNi0zNzMyMDVhZThmOTkiLCJodHRwOi8vMTAtNC5jb20vY2xhaW1zL3VzZXJJZCI6IjIzNTEiLCJpc3MiOiJodHRwczovL2Nsb3VkLjEwLTQuY29tIiwiYXVkIjoiaHR0cHM6Ly9jbG91ZC4xMC00LmNvbSIsImV4cCI6MjE0NzM5NTgxMCwibmJmIjoxNTE2MjQzODEwfQ.YtnGLxWij4sZ-NhAiEVLXgbUXrKZmsKb1uZn_ISH5GU'}
+    if OrderNumber is None:
+        return None
+    # r = requests.get(
+    #   'https://cloud.10-4.com/atlas/frexapi/shipment/' + OrderNumber )
+    r = requests.get(url, headers=headers)
+    json_object = r.json()
+    ShareUrl = json_object['ShareUrl']
+    # for i in range(0, 30):
+    #     if date in weather[i]['dt_txt']:
+    #         condition = weather[i]['weather'][0]['description']
+    #         break
+    speech = "The Order status can be obtained by clicking the below URL :: " + ShareUrl
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "source": "apiai-getOrderStatus-webhook"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
